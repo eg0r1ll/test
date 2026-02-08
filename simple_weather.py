6480d6915b21c077abb16d2b2ddb8922c3962cc0
eg0r1ll <steamxzakat9@gmail.com>
2026-02-08 17:31:50 +0300

    updated simple weather

--- simple_weather.py
+++ simple_weather.py
@@ -1,7 +1,7 @@
-import requests
+import requests # python -m pip install requests
 
 
-def rabin_karp_search(text, pattern):
+def rabin_karp_search(text, pattern): # Robin Karp's algorithm
     pattern_hash = hash(pattern)
     window_hash = hash(text[:len(pattern)])
 
@@ -12,29 +12,35 @@ def rabin_karp_search(text, pattern):
     return None
 
 
-graduse = '°'
+# I found this in the HTML code (https://yandex.ru/pogoda)
 patternTemp = "AppFactTemperature_value"
 patternPlusMinus = "AppFactTemperature_sign__1MeN4 AppFactTemperature_attr__8pcxc"
 
 while True:
     location = input("location: ").lower()
-    r = requests.get(f"https://yandex.ru/pogoda/ru/{location}")
-    data = r.text
+    if location == "exit" or location == "quit": break
 
+    request = requests.get(f"https://yandex.ru/pogoda/ru/{location}") # get the page markup
+    data = request.text[70000:75000:1] # cut off the excess
+    del request
+
+    # Get the index where the pattern match was found:
     temperature_idx = rabin_karp_search(data, patternTemp)
     sign_idx = rabin_karp_search(data, patternPlusMinus)
     
     if temperature_idx:
-        temperature = data[temperature_idx+33:temperature_idx+35:1]
+        # search for temperature value:
+        temperature = data[temperature_idx+33:temperature_idx+35:1] # for a two-digit number
         if not temperature[-1].isdigit():
-            temperature = data[temperature_idx+33:temperature_idx+34:1]
+            temperature = data[temperature_idx+33:temperature_idx+34:1] # for a single digit number
 
+        # get the sign before the temperature (+/- or if zero then nothing):
         if temperature != '0':
             sign = data[sign_idx+63:sign_idx+64:1]
         else:
             sign = ''
 
-        print(f"It's {sign}{temperature}{graduse} in {location.capitalize()} now")
+        print(f"It's {sign}{temperature}° in {location.capitalize()} now") # output
 
     else:
-        print("Местоположение не найденно :(")
+        print("Location not found :(")
