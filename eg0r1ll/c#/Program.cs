ae8ac5ac3f45a3d5b2ed4551964e4df622fb93bc
eg0R1ll <steamxzakat9@gmail.com>
2026-02-23 23:19:40 +0300

    added new files

--- eg0r1ll/c#/Program.cs
+++ eg0r1ll/c#/Program.cs
@@ -6,53 +6,91 @@ namespace Program
 	{
 		static void Main()
 		{
-			int[,] array = new int[6, 5];
-			Random rand = new Random();
-			int sum_1 = 0;
+		//	int[,] array = new int[6, 5];
+		//	Random rand = new Random();
+		//	int sum_1 = 0;
 
-			// Заполнение массива:
-			for(int i = 0; i < array.GetLength(0); ++i)
-			{
-				for (int j = 0; j < array.GetLength(1); ++j)
-				{
-					array[i, j] = rand.Next(1, 5);
-					sum_1 += array[i, j];
-				}
-			}
+		//	// Заполнение массива:
+		//	for(int i = 0; i < array.GetLength(0); ++i)
+		//    {
+		//		for (int j = 0; j < array.GetLength(1); ++j)
+		//		{
+		//			array[i, j] = rand.Next(1, 5);
+		//			sum_1 += array[i, j];
+		//		}
+		//	}
 
-			// Вывод массива и суммы строк
-			for(int i = 0; i < array.GetLength(0); ++i)
-			{
-				int temp_sum = 0;
-				for(int j = 0; j < array.GetLength(1); ++j)
-				{
-					temp_sum += array[i, j];
-					
-					if (j == array.GetLength(1) - 1)
-					{
-						
-						Console.Write($"{array[i, j]}\t{temp_sum}");
-						continue;
-					}
-					
-					Console.Write($"{array[i, j]}\t");
-				}
-				Console.WriteLine();				
-			}
+		//	// Вывод массива и суммы строк
+		//	for(int i = 0; i < array.GetLength(0); ++i)
+		//	{
+		//		int temp_sum = 0;
+		//		for(int j = 0; j < array.GetLength(1); ++j)
+		//		{
+		//			temp_sum += array[i, j];
+		//			
+		//			if (j == array.GetLength(1) - 1)
+		//			{
+		//				
+		//				Console.Write($"{array[i, j]}\t{temp_sum}");
+		//				continue;
+		//			}
+		//			
+		//			Console.Write($"{array[i, j]}\t");
+		//		}
+		//		Console.WriteLine();				
+		//	}
 
-			// Вывод суммы столбцов:
-			Console.WriteLine();
-			for(int i = 0; i < array.GetLength(1); ++i)
-			{
-				int temp_sum = 0;
-				for (int j = 0; j < array.GetLength(0); ++j)
-				{
-					temp_sum += array[j, i];
-				}
-				Console.Write($"{temp_sum}\t");
-			}		
-			
-			Console.WriteLine(sum_1); // Вывод суммы всех значений массива
+		//	// Вывод суммы столбцов:
+		//	Console.WriteLine();
+		//	for(int i = 0; i < array.GetLength(1); ++i)
+		//	{
+		//		int temp_sum = 0;
+		//		for (int j = 0; j < array.GetLength(0); ++j)
+		//		{
+		//			temp_sum += array[j, i];
+		//		}
+		//		Console.Write($"{temp_sum}\t");
+		//	}		
+		//	
+		//    Console.WriteLine(sum_1); // Вывод суммы всех значений массива
+
+            
+    
+            // sorts
+
+            //int[] array = {3, 2, 1, 4, 5};
+            //int temp;
+
+            //// bubble sort:
+            //for(int i = 0; i < array.Length; ++i)
+            //{
+            //    for(int j = i; j < array.Length - i; ++j)
+            //    {
+            //        if(array[i] > array[j])
+            //        {
+            //            temp = array[i];
+            //            array[i] = array[j];
+            //            array[j] = temp;
+            //        }
+            //    }
+            //}
+
+            //// output:
+            //for(int i = 0; i < array.Length; ++i)
+            //{
+            //    Console.Write($"{array[i]} ");
+            //}
+            //Console.WriteLine();
+
+
+            //Hoare sorting
+
+
+            
+
+            
+
+
 		}
 	}
 }
