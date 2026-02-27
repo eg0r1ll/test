b4fe9a9a7aad5d96e6d6c9f0bcc39e363b5a3ed6
eg0R1ll <steamxzakat9@gmail.com>
2026-02-27 21:51:06 +0300

    -pygame

--- eg0r1ll/pygame/test_func.py
+++
@@ -1,7 +0,0 @@
-import pygame
-import random
-
-
-def testFoo(screen, countt=0):
-    pygame.draw.rect(screen, (random.randint(0,255), random.randint(0,255), random.randint(0,255)), (0, 0, 100, 100))
-    print("test")
